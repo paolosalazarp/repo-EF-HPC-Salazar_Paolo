df37694a1e5ed242f56186a3c91b8b5d7602cecc
Paolo César Salazar Patricio <pc.salazarp@alum.up.edu.pe>
2025-06-30 16:17:36 +0000

    hola

--- app_core.py
+++ app_core.py
@@ -2,15 +2,17 @@ import numpy
 import multiprocessing
 from multiprocessing import Pool,Process
 def calcular_factorial(n):
+    print('Calculando factorial..')
     resultado=1
     for i in range(1,n+1):
         resultado=resultado*i
-    print(resultado)
+    print('Resultado factorial: ',resultado)
 def calcular_multiplicacion(lista):
+    print('Calculando multiplicacion')
     resultado=1
     for i in lista:
         resultado=resultado*i
-    print(resultado)
+    print('Resultado factorial: ',resultado)
 def main():
     proceso1=multiprocessing.Process(calcular_factorial(10))
     proceso2=multiprocessing.Process(calcular_multiplicacion([2,4,6,10,5]))
@@ -18,4 +20,5 @@ def main():
     proceso2.start()
     proceso1.join()
     proceso2.join()
+    print('Tareas completadas')
 main()
